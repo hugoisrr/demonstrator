c479514624d8ad3b60d1bae3c986dbe487343ef9
Hugo Ramirez <hugo@pop-os.localdomain>
2020-02-14 15:55:19 +0100

    Jquery functions - inital components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import 'bootstrap/dist/js/bootstrap'
 import 'chart.js/dist/Chart'
 import 'datatables-bootstrap/js/dataTables.bootstrap'
 
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'))
