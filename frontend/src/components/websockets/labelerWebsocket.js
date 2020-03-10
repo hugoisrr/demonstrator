545f0e0a83b54ac97f3c79225223f62d314724f9
Paul <paul.nicolas@ipa.fraunhofer.de>
2020-03-10 14:20:32 +0100

    Labeler part

--- frontend/src/components/websockets/labelerWebsocket.js
+++ frontend/src/components/websockets/labelerWebsocket.js
@@ -1,24 +1,29 @@
 import { useContext } from 'react'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
 import LabelerContext from '../../context/labeler/labelerContext'
-import WebsocketContext from '../../context/websocket/websocketContext'
+
 
 //const client = new W3CWebSocket('ws://172.21.30.241:3000')
 export const client = new W3CWebSocket('ws://localhost:3000')
 
 const LabelerWebsocket = () => {
 	const labelerContext = useContext(LabelerContext)
-	const { getLabelerData, getLabelerWks } = labelerContext
-	const websocketContext = useContext(WebsocketContext)
-	const { websocketLabelerOpen } = websocketContext
+	const {
+		getLabelerData,
+		getLabelerWks,
+		pushToDictionary,
+		startDictionary,
+		getLabelerWebsocketStatus,		
+	} = labelerContext
 
 	client.onerror = () => {
 		console.error('Connection Error with WebSocket Labeler')
+		getLabelerWebsocketStatus('ERROR')
 	}
 
 	client.onopen = () => {
 		console.log('WebSocket Labeler Client Connected')
-		websocketLabelerOpen()
+		getLabelerWebsocketStatus('CONNECTING')
 	}
 
 	client.onmessage = message => {
@@ -26,12 +31,16 @@ const LabelerWebsocket = () => {
 		if (Array.isArray(message) && message.length > 0) {
 			getLabelerWks(message)
 		} else if (typeof message === 'object') {
+			startDictionary()
 			getLabelerData(message)
+			pushToDictionary(message)
+			getLabelerWebsocketStatus('OPEN')
 		}
 	}
 
 	client.onclose = () => {
 		console.log('WebSocket Labeler Client Closed')
+		getLabelerWebsocketStatus('CLOSED')
 	}
 
 	return null
