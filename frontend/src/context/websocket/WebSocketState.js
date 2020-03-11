7c0eadced8a064d20549a258a9d31939fc205c02
Paul <paul.nicolas@ipa.fraunhofer.de>
2020-03-11 10:58:29 +0100

    Delete Labeler Part Websocket Folder

--- frontend/src/context/websocket/WebSocketState.js
+++ frontend/src/context/websocket/WebSocketState.js
@@ -2,14 +2,11 @@
 import React, { useReducer } from 'react'
 import WebsocketContext from './websocketContext'
 import WebsocketReducer from './websocketReducer'
-import { OPEN_WEBSOCKET_LABELER, OPEN_WEBSOCKET_DEVICE } from '../types'
+import { OPEN_WEBSOCKET_DEVICE } from '../types'
 
 const WebSocketState = props => {
 	//store in state if each websocket is either open or closed
 	const initialState = {
-		labelerWebsocket: {
-			open: false,
-		},
 		deviceWebsocket: {
 			open: false,
 		},
@@ -19,15 +16,12 @@ const WebSocketState = props => {
 
 	//Change 'open' attribute of each websocket to true when each websocket triggers a 'onconnect' message
 
-	const websocketLabelerOpen = () => dispatch({ type: OPEN_WEBSOCKET_LABELER })
-
 	const websocketDeviceOpen = () => dispatch({ type: OPEN_WEBSOCKET_DEVICE })
 
 	return (
 		<WebsocketContext.Provider
 			value={{
-				labelerWebsocket: state.labelerWebsocket,
-				websocketLabelerOpen,
+
 				deviceWebsocket: state.deviceWebsocket,
 				websocketDeviceOpen,
 			}}
--- frontend/src/context/websocket/websocketReducer.js
+++ frontend/src/context/websocket/websocketReducer.js
@@ -1,14 +1,8 @@
-import { OPEN_WEBSOCKET_DEVICE, OPEN_WEBSOCKET_LABELER } from '../types'
+import { OPEN_WEBSOCKET_DEVICE } from '../types'
 
 //Change 'open' attribute of each websocket to true when each websocket triggers a 'onconnect' message
 export default (state, action) => {
 	switch (action.type) {
-		case OPEN_WEBSOCKET_LABELER:
-			return {
-				...state,
-				open: (state.labelerWebsocket.open = true),
-			}
-
 		case OPEN_WEBSOCKET_DEVICE:
 			return {
 				...state,
