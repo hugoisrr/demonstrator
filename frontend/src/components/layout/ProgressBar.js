f12847a435c292152a1daaa50808125942157eb4
Hugo Ramirez <hugo@pop-os.localdomain>
2020-03-11 10:06:02 +0100

    comments in progressbar

--- frontend/src/components/layout/ProgressBar.js
+++ frontend/src/components/layout/ProgressBar.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 
 const ProgressBar = ({ states, data }) => {
 	const counterStates = new Map()
+	// Assigns states id's as keys and empty arrays as values
 	for (const [stateId] of Object.keys(states)) {
 		counterStates.set(stateId, [])
 	}
+	// For each state id an element of the data in been pushed into an empty array
 	data.forEach(element => {
 		for (const [state_id, statesArray] of counterStates.entries()) {
 			if (element === parseInt(state_id)) {
@@ -13,13 +15,38 @@ const ProgressBar = ({ states, data }) => {
 			}
 		}
 	})
+	// According to the length of the array is the number of elements pushed and counts the total
 	let total = 0
 	for (const [state_id, statesArray] of counterStates.entries()) {
 		console.log(state_id, '->', statesArray.length)
 		total += statesArray.length
 	}
 	console.log('total:', total)
-	return <div>ProgressBar</div>
+	return (
+		<div className='progress'>
+			<div
+				className='progress-bar progress-bar-success'
+				role='progressbar'
+				style={{ flex: 0.4 }}
+			>
+				Free Space
+			</div>
+			<div
+				className='progress-bar progress-bar-warning'
+				role='progressbar'
+				style={{ flex: 0.1 }}
+			>
+				Warning
+			</div>
+			<div
+				className='progress-bar progress-bar-danger'
+				role='progressbar'
+				style={{ flex: 0.2 }}
+			>
+				Danger
+			</div>
+		</div>
+	)
 }
 
 ProgressBar.propTypres = {
