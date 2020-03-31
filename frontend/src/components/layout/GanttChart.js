7d40014e786bd9310e25076b2439b409790cd0c8
Paul <paul.nicolas@ipa.fraunhofer.de>
2020-03-31 16:06:17 +0200

    Clean up the gantt chart code

--- frontend/src/components/layout/GanttChart.js
+++ frontend/src/components/layout/GanttChart.js
@@ -3,37 +3,39 @@ import ModelContext from '../../context/model/modelContext'
 import { Spinner } from '../layout/Spinner'
 import * as d3 from "d3";
 
+// svg_x (int) -> as wide as the parent div/card
+// svg_y (int) -> hight can be fix
+// states_length (int) -> how many states
+// states_colors[] -> color matching to each states
+// state_history[(int)] -> last X data points
+// state_history_length (int) -> X -> length of the history
+
+
 export const GanttChart = ({ ws_id, data }) => {
 	const modelContext = useContext(ModelContext)
-  	var margin = { top: 20, right: 10, bottom: 30, left: 10 };
-  	var width = 10 - margin.left - margin.right; //TODO: Dynamic
-	var height = 200 - margin.top - margin.bottom; //fix
+  
 	// creating a non existing svg
 	var svg = null;
 	// input data for graph
 	// array of length 30 filled with -1, adding state values from websocket	
 	var i = 0
 	var cardData = Array.from({ length: 30 }, () => data[i++]); //TODO: websocket data 
-		
-	
+
 	// #################################################
 	// find the element, take the reference and start next step in the chain
 		var w= 10;
 		var h= 50;
-		var gantt_scale_Y = d3.scaleLinear()
-		.domain([0, 2]) // input
-		.range([0, 2]);
 		//var gantt_scale_Y = d3.scaleLinear().domain([0,2]).range([0,2]);
 		/* var gantt_scale_X = d3.scaleLinear()
 		.domain([0, width])
 		.range([0,60]); */
 
 		//differentiate each svg graph according to its workstation id
-		var svg = d3.select("#gantt_chart" + ws_id).selectAll("rect").remove()
-		var svg = d3.select("#gantt_chart" + ws_id).selectAll("rect")
-		.data(cardData)
+		svg = d3.select("#gantt_chart" + ws_id).selectAll("rect").remove()
+		svg = d3.select("#gantt_chart" + ws_id).selectAll("rect")
+		.data(cardData) //[0,2,1,1,1,0,0,0,0,1,1,1,2,2,2] length=12
 		.enter()
-		.append("rect")
+		.append("rect") //value of cardData[0]=0
 		.attr("fill", function (d) {
 			if (d == 0) {
 				return "red"
@@ -52,19 +54,16 @@ export const GanttChart = ({ ws_id, data }) => {
 				}
 			)	//fix size
 			.attr("x", function(d,i){ // dynamic position
-				return i*(w+1); // [index of array] * [width of rect] + [gap]
+				return i*(w+1); // [index of array] * [width of rect] + [gap] 0,11,22,33,44,...
 			})
 			.attr("y", function(d){ // dynamic position
-				return h+(h*d); // [height of rect] + ( [height of rect] * [array value] )
+				return h+(h*d); // [height of rect] + ( [height of rect] * [array value] ) 50,100,150,...
 			})
 	return (
 		<Fragment>
 			<div id='content'>
 				<div>
 					<svg id={"gantt_chart" + ws_id }
-						ref={el => {
-						if (!el) return;
-						}} 
 						style={{height: 300, width: window.width }}> 
 					</svg>
 				</div>
