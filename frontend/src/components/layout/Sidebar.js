5bfc928b161776e0a8227da8e85e3c9cdd862f25
Paul_N28 <paulnicolas2809@gmail.com>
2020-04-30 10:42:57 +0200

    Sidebar without placeholders + Gantt Chart Responsive

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -16,6 +16,7 @@ const Sidebar = () => {
   const modelWks = modelContext.wks;
   const deviceWks = deviceContext.wks;
 
+
   var labelerId = [];
   labelerWks.forEach((element) => {
     labelerId.push(element.ws_id);
@@ -34,47 +35,74 @@ const Sidebar = () => {
   var allWks = [];
   allWks = allWks.concat(labelerWks, modelWks, deviceWks);
 
+  
   if (allWks.length > 0) {
     allWks = allWks.map((item, i) => {
-      return (({ ws_id, ws_name, source }) => ({ ws_id, ws_name, source }))(
+      return (({ ws_id}) => ({ ws_id}))(
         allWks[i]
       );
     });
 
-  const uniqueWks = [...new Set(allWks.map((item) => item.ws_id))]
+    const uniqueWks = [...new Set(allWks.map((item) => item.ws_id))];
 
-  const uniqueDict = []
-  uniqueWks.forEach(element => {
-    uniqueDict.push({key : element, ws_id : element, labeler : false, model : false, device : false})
-  });
+    const uniqueDict = [];
+    uniqueWks.forEach((element) => {
+      uniqueDict.push({
+        key: element,
+        ws_id: element,
+        labeler: false,
+        model: false,
+        device: false,
+      });
+    }); 
 
-
-  for (var key in uniqueDict) {
-    if (uniqueDict.hasOwnProperty(key)) {           
-      let current_element = uniqueDict[key]
-      if (labelerId.includes(current_element['ws_id'])){
-        current_element['labeler'] = true
+    for (var key in uniqueDict) {
+      if (uniqueDict.hasOwnProperty(key)) {
+        let current_element = uniqueDict[key];
+        if (labelerId.includes(current_element["ws_id"])) {
+          current_element["labeler"] = true;
+        }
+        if (modelId.includes(current_element["ws_id"])) {
+          current_element["model"] = true;
+        }
+        if (deviceId.includes(current_element["ws_id"])) {
+          current_element["device"] = true;
+        }
       }
-      if (modelId.includes(current_element['ws_id'])){
-        current_element['model'] = true
-      }
-      if (deviceId.includes(current_element['ws_id'])){
-        current_element['device'] = true
-      } 
-  }
-} 
+    }
 
-function Display(param){
-  console.log(param)
-  if (param['param']){
-    return  <td><svg height="30" width="30">
-    <circle cx="15" cy="15" r="10" stroke="black" stroke-width="2" fill="green" />
-  </svg> </td>
-  }
-  return <td><svg height="30" width="30">
-  <circle cx="15" cy="15" r="10" stroke="black" stroke-width="2" fill="red" />
-</svg> </td>
-}
+    function Display(param) {
+      if (param["param"]) {
+        return (
+          <td>
+            <svg height="30" width="30">
+              <circle
+                cx="15"
+                cy="15"
+                r="10"
+                stroke="black"
+                stroke-width="2"
+                fill="green"
+              />
+            </svg>
+          </td>
+        );
+      }
+      return (
+        <td>
+          <svg height="30" width="30">
+            <circle
+              cx="15"
+              cy="15"
+              r="10"
+              stroke="black"
+              stroke-width="2"
+              fill="red"
+            />
+          </svg>
+        </td>
+      );
+    }
 
     return (
       <ul
@@ -132,7 +160,7 @@ function Display(param){
           </div>
         </li>
 
-        <li className="nav-item">
+{/*       <li className="nav-item">
           <a
             className="nav-link collapsed"
             href="/#"
@@ -166,27 +194,37 @@ function Display(param){
               </a>
             </div>
           </div>
-        </li>
+        </li> */}
 
-        <hr className="sidebar-divider d-none d-md-block" />
+<hr className="sidebar-divider d-none d-md-block" />
+        <li className="nav-item">   
+        <div className="container">
         <table>
-        <tr>
-          <th> ID </th>
-          <th> Labeler </th>
-          <th> Model </th>
-          <th> Device </th>
-        </tr>
-        {uniqueDict.map((item) => {
-          return (
-            <tr>                
-                <td> {item['ws_id']} </td>
-                <Display param = {item['labeler']}></Display>
-                <Display param = {item['model']}></Display>
-                <Display param = {item['device']}></Display>
+          <thead>
+            <tr>
+            <th> ID </th>
+            <th> Labeler </th>
+            <th> Model </th>
+            <th> Signal </th>
             </tr>
-          )
-        })}
+          </thead>
+          {uniqueDict.map((item) => {
+            return (
+                <tbody>
+                  <tr>
+                    <td> {item["ws_id"]} </td>
+                    <Display param={item["labeler"]}></Display>
+                    <Display param={item["model"]}></Display>
+                    <Display param={item["device"]}></Display>
+                   </tr>
+                </tbody>
+
+            );
+          })}
         </table>
+        </div>
+        </li>
+        
 
         <hr className="sidebar-divider d-none d-md-block" />
 
--- frontend/src/components/layout/workstation/GanttChart.js
+++ frontend/src/components/layout/workstation/GanttChart.js
@@ -10,13 +10,25 @@ import * as d3 from 'd3'
 
 export const GanttChart = ({ ws_id, data, statesColors }) => {
 	// input data for graph
-	// array of length 30 filled with -1, adding state values from websocket
+	// array of length 50 filled with -1, adding state values from websocket
 	let i = 0
-	const cardData = Array.from({ length: 30 }, () => data[i++])
+	let dataPoints = 50
+	const cardData = Array.from({ length: dataPoints }, () => data[i++])
 
-	/* Rectangle size on each row */
-	const width = 10
+	/* Representing the svg width by getting the width of the parent div */
+	var svg_width = 0
+	var svg_parent = document.querySelector("#svg")
+	if (svg_parent){
+		svg_width = svg_parent.clientWidth
+	}
+
+	/* Rectangle size on each row 
+	Width = existing space / maximum length of the data Points 
+	Height has to be responsive in the future if we got more states for example */
+	var width = svg_width / dataPoints
 	const height = 50
+
+	/* Scale */
 
 	//differentiate each svg graph according to its workstation id
 	let svg = d3
@@ -38,7 +50,7 @@ export const GanttChart = ({ ws_id, data, statesColors }) => {
 				return statesColors[2]
 			} else if (value === 3) return statesColors[3]
 		})
-		.attr('width', width) //fix size
+		.attr('width', width) 
 		.attr('height', function(value) {
 			if (value === -1) {
 				return 0
@@ -46,7 +58,7 @@ export const GanttChart = ({ ws_id, data, statesColors }) => {
 		}) //fix size
 		.attr('x', function(d, i) {
 			// dynamic position
-			return i * (width + 1) // [index of array] * [width of rect] + [gap] 0,11,22,33,44,...
+			return i * width // [index of array] * [width of rect]  
 		})
 		.attr('y', function(value) {
 			// dynamic position
@@ -54,12 +66,12 @@ export const GanttChart = ({ ws_id, data, statesColors }) => {
 		})
 	return (
 		<Fragment>
-			<div className='text-center'>
+			<div className='text-center' id = 'svg'>
 				<svg
 					id={'gantt_chart' + ws_id}
 					style={{
 						height: 300,
-						width: window.width,
+						width: '100%',
 						border: '1px solid white',
 					}}
 				></svg>
